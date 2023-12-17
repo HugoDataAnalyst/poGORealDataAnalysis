9a52c75a8553bf67590c4a6927fb72415bda3958
hugo gomes <gothugo21@gmail.com>
2023-12-17 18:56:11 +0000

    push produc dash

--- Dash/stats_pogo_dash.py
+++ Dash/stats_pogo_dash.py
@@ -1149,6 +1149,6 @@ app.clientside_callback(
 
 
 if __name__ == '__main__':
-    #app.run_server(debug=False, port=8065)    
+    app.run_server(debug=False, port=8065)    
     #app.run_server(debug=False, host='0.0.0.0', port=8065)
-    app.run_server(debug=True, port=8065)
+    #app.run_server(debug=True, port=8065)
